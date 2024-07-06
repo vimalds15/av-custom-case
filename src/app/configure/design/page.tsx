276cac88a77dfdc00fe25e1e2761c13edb9cfa07
vimalds15 <antovimalands@gmail.com>
2024-07-06 11:39:03 +0530

    feat:"av case project"

---
+++ src/app/configure/design/page.tsx
@@ -0,0 +1,37 @@
+import { db } from "@/db";
+import { notFound } from "next/navigation";
+import DesignConfigurator from "./DesignConfigurator";
+
+interface PageProps {
+  searchParams: {
+    [key: string]: string | string[] | undefined;
+  };
+}
+
+const DesignPage = async ({ searchParams }: PageProps) => {
+  const { id } = searchParams;
+
+  if (!id || typeof id !== "string") {
+    return notFound();
+  }
+
+  const configuration = await db.configuration.findUnique({
+    where: { id },
+  });
+
+  if (!configuration) {
+    return notFound();
+  }
+
+  const { height, width, imageUrl } = configuration;
+
+  return (
+    <DesignConfigurator
+      configId={configuration.id}
+      imageDimensions={{ width, height }}
+      imageUrl={imageUrl}
+    />
+  );
+};
+
+export default DesignPage;
